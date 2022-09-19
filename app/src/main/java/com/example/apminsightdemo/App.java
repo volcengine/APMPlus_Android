0177170464b459f6e328646fb42cf34282b7de83
wangtao <wangtao.steven@bytedance.com>
2022-09-19 22:11:31 +0800

    change version 1.4.8

--- app/src/main/java/com/example/apminsightdemo/App.java
+++ app/src/main/java/com/example/apminsightdemo/App.java
@@ -1,18 +1,12 @@
 package com.example.apminsightdemo;
 
 import android.app.Application;
-import android.support.annotation.Nullable;
-
-import com.apm.insight.AttachUserData;
-import com.apm.insight.CrashType;
 import com.apm.insight.MonitorCrash;
 import com.apm.insight.log.VLog;
 import com.bytedance.apm.insight.ApmInsight;
 import com.bytedance.apm.insight.ApmInsightInitConfig;
 import com.bytedance.apm.insight.IDynamicParams;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Application
@@ -37,21 +31,32 @@ public class App extends Application {
      * ApmInsight崩溃监控初始化
      */
     private void initCrash() {
-        MonitorCrash crash = MonitorCrash.init(this, "187277", 1, "1.0.0")
-                .setCustomDataCallback(new AttachUserData() {
-            @Nullable
-            @Override
-            public Map<? extends String, ? extends String> getUserData(CrashType type) {
-                HashMap<String, String> map = new HashMap<>();
-                map.put("app_custom", "app_value");
-                return map;
-            }
-        });
-        crash.config().setChannel("channel");
-//        crash.config().setDeviceId("did");//可选，可以设置自定义did,不设置会使用内部默认的
-//        crash.setReportUrl("www.xxx.com"); // 私有化部署：私有化部署才配置上报地址
-        crash.addTags("key", "value"); // 自定义筛选tag, 按需添加、可多次覆盖
-
+        MonitorCrash.Config config = MonitorCrash.Config.app("187277")
+                .token("app_token")// 设置鉴权token，可从平台应用信息处获取，token错误无法上报数据
+//              .versionCode(1)// 可选，默认取PackageInfo中的versionCode
+//              .versionName("1.0")// 可选，默认取PackageInfo中的versionName
+//              .channel("test")// 可选，设置App发布渠道，在平台可以筛选
+//              .url("www.xxx.com")// 默认不需要，私有化部署才配置上报地址
+                //可选，可以设置自定义did，不设置会使用内部默认的
+//              .dynamicParams(new MonitorCrash.Config.IDynamicParams() {
+//                  @Override
+//                  public String getDid() {//返回空会使用内部默认的did
+//                      return null;
+//                  }
+//
+//                  @Override
+//                  public String getUserId() {
+//                      return null;
+//                  }
+//              })
+                //可选，添加业务自定义数据，在崩溃详情页展示
+//              .customData(crashType -> {
+//                  HashMap<String, String> map = new HashMap<>();
+//                  map.put("app_custom", "app_value");
+//                  return map;
+//              })
+                .build();
+        MonitorCrash monitorCrash = MonitorCrash.init(this, config);
     }
 
     /**
@@ -59,8 +64,10 @@ public class App extends Application {
      */
     private void initApmInsight() {
         ApmInsightInitConfig.Builder builder = ApmInsightInitConfig.builder();
-        //设置分配的appid
+        //必填：设置分配的appid
         builder.aid("187277");
+        //必填：设置平台的app_token
+        builder.token("app_token");
         //是否开启卡顿功能
         builder.blockDetect(true);
         //是否开启严重卡顿功能
@@ -85,6 +92,8 @@ public class App extends Application {
         builder.enableLogRecovery(true);
         //控制是否打开cpu监控能力
         builder.cpuMonitor(true);
+        //打开磁盘监控
+        builder.diskMonitor(true);
         //设置数据和Rangers Applog数据打通，设备标识did必填。1.3.16版本增加接口
         builder.setDynamicParams(new IDynamicParams() {
             @Override
